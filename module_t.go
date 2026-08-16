9f0ffdded394e92d9054ece95173f6556b026bf1
kge <kge@bitwormhole.com>
2026-08-16 15:47:32 +0800

    up to v0.10.1

--- module_t.go
+++ module_t.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/libgorm"
-	theModuleVersion  = "v0.10.0"
-	theModuleRevision = 18
+	theModuleVersion  = "v0.10.1"
+	theModuleRevision = 19
 )
 
 ////////////////////////////////////////////////////////////////////////////////
